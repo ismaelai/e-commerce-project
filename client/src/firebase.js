0709054ac6412369ce394ddc51411c261a2ebf32
Ismael Gonzalez <ismamontiel@hotmail.com>
2020-12-03 15:00:00 +0100

    registration validated. Toast notification, Setup REDUX

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+
+// Your web app's Firebase configuration
+ // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAbVHGwobTLaxnpjlGWy06imYTJr2mFDaU",
+    authDomain: "e-commerce-4e4a4.firebaseapp.com",
+    databaseURL: "https://e-commerce-4e4a4.firebaseio.com",
+    projectId: "e-commerce-4e4a4",
+    storageBucket: "e-commerce-4e4a4.appspot.com",
+    messagingSenderId: "1088320243458",
+    appId: "1:1088320243458:web:d229900d1a06802ad9641b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
